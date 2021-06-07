e45c1afe4d36f1918e03e471511b6e26f3941cee
Maxwell Meyer <maxreadswell@gmail.com>
2021-06-07 07:27:56 -0700

    add get method to return results by ID

--- Controllers/AnimalsController.cs
+++ Controllers/AnimalsController.cs
@@ -33,5 +33,17 @@ namespace CretaceousPark.Controllers
 
       return CreatedAtAction("Post", new { id = animal.AnimalId }, animal);
     }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Animal>> GetAnimal(int id)
+    {
+      var animal = await _db.Animals.FindAsync(id);
+
+      if (animal == null)
+      {
+        return NotFound();
+      }
+
+      return animal;
+    }
   }
 }
